f0ac781fe3075eeac2e40778242f9b9e924f6caf
zeyuanzhong <zeyuanzhong@sina.com>
2014-03-14 14:24:47 +0800

    Update movedatabase.sh

--- movedatabase.sh
+++ movedatabase.sh
@@ -2,10 +2,10 @@
 
 set date [exec date "+%Y-%m-%d.%H:%M:%S"]
 set timeout 10
-spawn scp -r root@219.217.191.100:/backup$date.sql /backup/
+spawn scp -r root@***.***.***.***:/backup$date.sql /backup/
 set timeout 10
 expect { "yes/no" {send "yes\r";exp_continue} }
-expect "root@219.217.191.100's password:"
+expect "root@***.***.***.***'s password:"
 set timeout 10
 send "parabloa\r"
 set timeout 200
